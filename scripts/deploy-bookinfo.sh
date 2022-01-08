ed82014211a855d402185b2152f71031a51c1b89
Adam Zwickey <adam@tetrate.io>
2022-01-08 10:12:12 -0500

    moving to yq 4.x

--- scripts/deploy-bookinfo.sh
+++ scripts/deploy-bookinfo.sh
@@ -3,24 +3,24 @@
 
 echo "Deploying Bookinfo Application"
 kubectl create ns bookinfo
-kubectl apply -n bookinfo -f bookinfo/bookinfo.yaml
 kubectl apply -n bookinfo -f bookinfo/cluster-ingress-gw.yaml
+kubectl apply -n bookinfo -f bookinfo/bookinfo-multi.yaml
 kubectl -n bookinfo create secret tls bookinfo-certs \
-    --key $(yq r $VARS_YAML k8s.bookinfoCertDir)/privkey.pem \
-    --cert $(yq r $VARS_YAML k8s.bookinfoCertDir)/fullchain.pem
+    --key $(yq eval .k8s.bookinfoCertDir $VARS_YAML)/privkey.pem \
+    --cert $(yq eval .k8s.bookinfoCertDir $VARS_YAML)/fullchain.pem
 while kubectl get po -n bookinfo | grep Running | wc -l | grep 7 ; [ $? -ne 0 ]; do
     echo Bookinfo is not yet ready
-    sleep 5s
+    sleep 5
 done
-while kubectl get service tsb-gateway-bookinfo -n bookinfo | grep pending | wc -l | grep 0 ; [ $? -ne 0 ]; do
+while kubectl get service tsb-gateway -n bookinfo | grep pending | wc -l | grep 0 ; [ $? -ne 0 ]; do
     echo Gateway IP not assigned
-    sleep 5s
+    sleep 5
 done
-export GATEWAY_IP=$(kubectl get service tsb-gateway-bookinfo -n bookinfo -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-kubectl apply -f bookinfo/tmp.yaml
-for i in {1..50}
-do
-    curl -vv http://$GATEWAY_IP/productpage\?u=normal
-done
-kubectl delete -f bookinfo/tmp.yaml
-kubectl apply -n bookinfo -f bookinfo/bookinfo-multi.yaml
+# export GATEWAY_IP=$(kubectl get service tsb-gateway -n bookinfo -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# kubectl apply -f bookinfo/tmp.yaml
+# for i in {1..50}
+# do
+#     curl -vv http://$GATEWAY_IP/productpage\?u=normal
+# done
+# kubectl delete -f bookinfo/tmp.yaml
+# kubectl apply -n bookinfo -f bookinfo/bookinfo-multi.yaml
--- scripts/deploy-cp.sh
+++ scripts/deploy-cp.sh
@@ -2,34 +2,36 @@
 : ${VARS_YAML?"Need to set VARS_YAML environment variable"}
 : ${1?"Must supply cluster name arg"}
 
-CLUSTER_NAME=$1
+export CLUSTER_NAME=$1
 
 echo "Deploying Workload Cluster $CLUSTER_NAME Control Plane"
 kubectl create ns istio-system
-  kubectl create secret generic cacerts -n istio-system \
-    --from-file=$(yq r $VARS_YAML k8s.istioCertDir)/ca-cert.pem \
-    --from-file=$(yq r $VARS_YAML k8s.istioCertDir)/ca-key.pem \
-    --from-file=$(yq r $VARS_YAML k8s.istioCertDir)/root-cert.pem \
-    --from-file=$(yq r $VARS_YAML k8s.istioCertDir)/cert-chain.pem
-  kubectl apply -f generated/$CLUSTER_NAME/cp-operator.yaml
-  kubectl apply -f generated/$CLUSTER_NAME/cluster-certs.yaml
-  kubectl apply -f generated/$CLUSTER_NAME/cluster-secrets.yaml
-  while kubectl get po -n istio-system -l name=tsb-operator | grep Running | wc -l | grep 1 ; [ $? -ne 0 ]; do
-      echo TSB Operator is not yet ready
-      sleep 5s
-  done
-  sleep 30 # Dig into why this is needed
-  cp control-plane.yaml generated/$CLUSTER_NAME/
-  yq write generated/$CLUSTER_NAME/control-plane.yaml -i "spec.hub" $(yq r $VARS_YAML tetrate.registry)
-  yq write generated/$CLUSTER_NAME/control-plane.yaml -i "spec.telemetryStore.elastic.host" $(yq r $VARS_YAML gcp.mgmt.fqdn)
-  yq write generated/$CLUSTER_NAME/control-plane.yaml -i "spec.managementPlane.host" $(yq r $VARS_YAML gcp.mgmt.fqdn)
-  yq write generated/$CLUSTER_NAME/control-plane.yaml -i "spec.managementPlane.clusterName" $CLUSTER_NAME
-  kubectl apply -f generated/$CLUSTER_NAME/control-plane.yaml
-  #Edge Pod is the last thing to start
-  while kubectl get po -n istio-system -l app=edge | grep Running | wc -l | grep 1 ; [ $? -ne 0 ]; do
-      echo Istio control plane is not yet ready
-      sleep 5s
-  done
-  kubectl patch ControlPlane controlplane -n istio-system --patch '{"spec":{"meshExpansion":{}}}' --type merge
+kubectl create secret generic cacerts -n istio-system \
+  --from-file=$(yq eval .k8s.istioCertDir $VARS_YAML)/ca-cert.pem \
+  --from-file=$(yq eval .k8s.istioCertDir $VARS_YAML)/ca-key.pem \
+  --from-file=$(yq eval .k8s.istioCertDir $VARS_YAML)/root-cert.pem \
+  --from-file=$(yq eval .k8s.istioCertDir $VARS_YAML)/cert-chain.pem
+kubectl apply -f generated/$CLUSTER_NAME/cp-operator.yaml
+kubectl apply -f generated/$CLUSTER_NAME/cluster-certs.yaml
+kubectl apply -f generated/$CLUSTER_NAME/cluster-secrets.yaml
+while kubectl get po -n istio-system -l name=tsb-operator | grep Running | wc -l | grep 1 ; [ $? -ne 0 ]; do
+    echo TSB Operator is not yet ready
+    sleep 5
+done
+sleep 30 # Dig into why this is needed
+cp control-plane.yaml generated/$CLUSTER_NAME/
+export REGISTRY=$(yq eval .tetrate.registry $VARS_YAML)
+export MP=$(yq eval .gcp.mgmt.fqdn $VARS_YAML)
+yq e -i '.spec.hub=strenv(REGISTRY) |
+        .spec.telemetryStore.elastic.host=strenv(MP) |
+        .spec.managementPlane.host=strenv(MP) |
+        .spec.managementPlane.clusterName=strenv(CLUSTER_NAME)' generated/$CLUSTER_NAME/control-plane.yaml
+kubectl apply -f generated/$CLUSTER_NAME/control-plane.yaml
+#Edge Pod is the last thing to start
+while kubectl get po -n istio-system -l app=edge | grep Running | wc -l | grep 1 ; [ $? -ne 0 ]; do
+    echo Istio control plane is not yet ready
+    sleep 5
+done
+kubectl patch ControlPlane controlplane -n istio-system --patch '{"spec":{"meshExpansion":{}}}' --type merge
 
 
